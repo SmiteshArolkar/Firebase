8b57173c4e157089dfa515b5f189d7c8848e214b
ProgramingHub(dont know what i m dong :D) <111310544+Spooderman177@users.noreply.github.com>
2022-10-15 21:55:25 +0530

    SM- Adding Firebase to Project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Initializing Webpack')
+import { initializeApp } from 'firebase/app'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAf2JT859cNE3R1tVM80j5VoLxgbOfC8xw",
+    authDomain: "fir-test-615fb.firebaseapp.com",
+    projectId: "fir-test-615fb",
+    storageBucket: "fir-test-615fb.appspot.com",
+    messagingSenderId: "877236588346",
+    appId: "1:877236588346:web:6763ad433afe3c798bca03"
+  };
+
+  initializeApp(firebaseConfig);
